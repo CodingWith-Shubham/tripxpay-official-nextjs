027903e4e9c07d3c3fe35c6b9afa94160f870b2c
ritesh <riteshsaini1601@gmail.com>
2025-07-06 15:43:13 +0530

    Hydration error resolved too

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,11 +6,6 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
-        <head>
-            <link rel="icon" href="/logo.svg" />
-        </head>
-        <body>
         <div className="min-h-screen bg-gray-950 text-white relative overflow-hidden">
             <div className="absolute top-0 left-0 w-[500px] h-[300px] bg-gradient-to-br from-[#00ffb4]/40 to-transparent rotate-12 blur-[120px] rounded-[30%] pointer-events-none z-0" />
             <div className="absolute right-0 w-[550px] h-[300px] md:bg-gradient-to-tr from-yellow-400/30 to-transparent rotate-180 blur-[120px] rounded-[20%] pointer-events-none z-0 top-[1.5%]" />
@@ -20,7 +15,6 @@ export default function RootLayout({
                 <Footer />
             </div>
         </div>
-        </body>
-        </html>
+
     );
 }
