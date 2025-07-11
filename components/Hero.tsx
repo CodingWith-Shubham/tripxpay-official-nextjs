0668e1815b68c4dbed7668a3fa81a95616d439e8
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-07-11 21:04:51 +0530

    when clicked to the getstarted redirect to the login instead of the signup page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
         </p>
         <div className="flex flex-col sm:flex-row justify-center gap-4 ">
           <Link
-            href={isLoggedIn ? "/verified" : "/signup"}
+            href={isLoggedIn ? "/verified" : "/login"}
             className="font-inter font-medium md:font-semibold text-sm md:text-base bg-yellow-500 hover:bg-yellow-600 text-black py-2 px-6 mt-3 md:mt-0 rounded-full"
           >
             GET STARTED
